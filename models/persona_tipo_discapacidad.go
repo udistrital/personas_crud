64dd2286e94853c197a6b5164cafd112cbf12d43
Mafe Avendaño <mafe.ud@gmail.com>
2018-04-03 06:58:30 -0500

    Se agrega RelatedSel a modelo de persona_tipo_discapacidad

--- models/persona_tipo_discapacidad.go
+++ models/persona_tipo_discapacidad.go
@@ -47,7 +47,7 @@ func GetPersonaTipoDiscapacidadById(id int) (v *PersonaTipoDiscapacidad, err err
 func GetAllPersonaTipoDiscapacidad(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(PersonaTipoDiscapacidad))
+	qs := o.QueryTable(new(PersonaTipoDiscapacidad)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
