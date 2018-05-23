1f22c141c192ad8d9b52be5bf138f544dd30d396
Mafe Avendaño <mafe.ud@gmail.com>
2018-05-23 10:33:51 -0500

    Ajustes en la respuesta del post de atributo_ubicacion y valor_atributo_ubicacion

--- controllers/atributo_ubicacion.go
+++ controllers/atributo_ubicacion.go
@@ -38,7 +38,7 @@ func (c *AtributoUbicacionController) Post() {
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if _, err := models.AddAtributoUbicacion(&v); err == nil {
 			c.Ctx.Output.SetStatus(201)
-			c.Data["json"] = v
+			c.Data["json"] = models.Alert{Type: "success", Code: "S_201", Body: v}
 		} else {
 			alertdb := structs.Map(err)
 			var code string
--- controllers/valor_atributo_ubicacion.go
+++ controllers/valor_atributo_ubicacion.go
@@ -39,7 +39,7 @@ func (c *ValorAtributoUbicacionController) Post() {
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if _, err := models.AddValorAtributoUbicacion(&v); err == nil {
 			c.Ctx.Output.SetStatus(201)
-			c.Data["json"] = v
+			c.Data["json"] = models.Alert{Type: "success", Code: "S_201", Body: v}
 		} else {
 			alertdb := structs.Map(err)
 			var code string
