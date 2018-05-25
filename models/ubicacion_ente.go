09e09b121d55bd89108dea11fbd1f974bbbe1b10
Mafe Avendaño <mafe.ud@gmail.com>
2018-05-25 11:19:14 -0500

    Restricciones para el update de contacto_ente, ubicacion_ente y valor_atributo_ente

--- models/ubicacion_ente.go
+++ models/ubicacion_ente.go
@@ -130,7 +130,7 @@ func UpdateUbicacionEnteById(m *UbicacionEnte) (err error) {
 	// ascertain id exists in the database
 	if err = o.Read(&v); err == nil {
 		var num int64
-		if num, err = o.Update(m); err == nil {
+		if num, err = o.Update(m, "Lugar"); err == nil {
 			fmt.Println("Number of records updated in database:", num)
 		}
 	}
--- models/valor_atributo_ubicacion.go
+++ models/valor_atributo_ubicacion.go
@@ -129,7 +129,7 @@ func UpdateValorAtributoUbicacionById(m *ValorAtributoUbicacion) (err error) {
 	// ascertain id exists in the database
 	if err = o.Read(&v); err == nil {
 		var num int64
-		if num, err = o.Update(m); err == nil {
+		if num, err = o.Update(m, "Valor"); err == nil {
 			fmt.Println("Number of records updated in database:", num)
 		}
 	}
