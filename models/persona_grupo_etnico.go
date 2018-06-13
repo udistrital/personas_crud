d53dff3c60e3a3d0d81ad5f657c2c6f5ccde9f1c
Mafe Avendaño <mafe.ud@gmail.com>
2018-06-13 10:58:00 -0500

    Se agrega RelatedSel() a persona_grupo_etnico

--- models/persona_grupo_etnico.go
+++ models/persona_grupo_etnico.go
@@ -47,7 +47,7 @@ func GetPersonaGrupoEtnicoById(id int) (v *PersonaGrupoEtnico, err error) {
 func GetAllPersonaGrupoEtnico(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(PersonaGrupoEtnico))
+	qs := o.QueryTable(new(PersonaGrupoEtnico)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
