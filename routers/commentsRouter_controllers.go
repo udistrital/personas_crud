6d8a42ae0445f9ace590cf30f99d6dc964ad5c3e
damillano93 <damillano93@gmail.com>
2018-07-05 15:15:14 -0500

    Eliminacion de lo realcionado con ente, correcion modelos

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -247,14 +247,6 @@ func init() {
 			MethodParams: param.Make(),
 			Params: nil})
 
-	beego.GlobalControllerRouter["github.com/udistrital/personas_crud/controllers:PersonaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/personas_crud/controllers:PersonaController"],
-		beego.ControllerComments{
-			Method: "GetFull",
-			Router: `/full/`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams: param.Make(),
-			Params: nil})
-
 	beego.GlobalControllerRouter["github.com/udistrital/personas_crud/controllers:PersonaEstadoCivilController"] = append(beego.GlobalControllerRouter["github.com/udistrital/personas_crud/controllers:PersonaEstadoCivilController"],
 		beego.ControllerComments{
 			Method: "Post",
