c77d2d373f3f64a748eb61dc2695ad3cbb499d88
fernandotower <fernandotower@gmail.com>
2018-07-11 14:27:44 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export PERSONAS_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/citest/db/username --output text --query Parameter.Value)"
-  export PERSONAS_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/citest/db/password --output text --query Parameter.Value)"
+  export PERSONAS_CRUD__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/personas_crud/db/username --output text --query Parameter.Value)"
+  export PERSONAS_CRUD__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/citest/db/passw/personas_crud/db/passwordord --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
