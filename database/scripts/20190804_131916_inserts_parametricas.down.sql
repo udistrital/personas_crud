f0f6a0915fca09808a6c3dbbc53ed4724878f693
Mafe Avendaño <mafe.ud@gmail.com>
2019-10-10 09:52:30 -0500

    ajustando delete from

--- database/scripts/20190804_131916_inserts_parametricas.down.sql
+++ database/scripts/20190804_131916_inserts_parametricas.down.sql
@@ -3,4 +3,4 @@ delete from persona.grupo_etnico;
 delete from persona.genero;
 delete from persona.estado_civil;
 delete from persona.tipo_discapacidad;
---delete from persona.tipo_relacion_personas;
+delete from persona.tipo_relacion_personas;
